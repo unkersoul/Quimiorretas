f82c72f73cf8ca280ff66d0ab890ffa4cceb46ea
unkersoul <unkersoul@gmail.com>
2014-03-17 09:37:17 +0100

    first

---
+++ Assets/canon.cs
@@ -0,0 +1,27 @@
+	using UnityEngine;
+using System.Collections;
+namespace juego{
+public class canon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			Vector3 diff;
+			 float rotZ;
+			
+		
+				diff = bicho.posicionBicho.position- transform.position;
+				//normalize difference  
+				diff.Normalize();
+				
+				//calculate rotation
+				rotZ = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+				//apply to object
+				transform.rotation = Quaternion.Euler(0f, 0f, rotZ);
+			}
+}
+}
---
+++ Assets/escena.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace juego{
+public class escena : MonoBehaviour {
+		private bichicho bicho;
+	// Use this for initialization
+	void Start () {
+			gameObject.AddComponent ("bichicho");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+}
+
+
+
